7a7fe3b4b0410c7ce7192e37d35013fd7f00c31b
corproot\taavedo3 <Donato.Verardi@swisscom.com>
2021-12-17 15:28:33 +0100

    refaktor

--- 1-static_website/config.mk
+++ 1-static_website/config.mk
@@ -1,5 +1,5 @@
 ### AWS Configuration
 # This is the config which will be used by the 'deploy' and 'clean' make targets
-AWS_BUCKET_NAME="static-s3-bucket-2021-12-17"
-AWS_DOMAIN_NAME="lab.donato.cloudns.ph"
+AWS_BUCKET_NAME=static-s3-bucket-2021-12-17
+AWS_DOMAIN_NAME=lab.donato.cloudns.ph
 AWS_CERTIFICATE_ARN="arn:aws:acm:us-east-1:440111066673:certificate/1b375a7c-d312-4b59-a846-4ef13a8fd32d"
--- 2-ecs_dyn_website/config.mk
+++
@@ -1,8 +0,0 @@
-### AWS Configuration
-# This is the config which will be used by the 'deploy' and 'clean' make targets
-AWS_STACK_NAME=fargate
-AWS_IMAGE_NAME=nginx
-AWS_DOMAIN_NAME=static-website.donato.cloudns.ph
-AWS_CERTIFICATE_ARN="arn:aws:acm:us-east-1:440111066673:certificate/ae2b1b4f-aa3c-4a85-a0cb-782579d6caa9"
-AWS_REGION=eu-west-3
-AWS_ACCOUNT_ID=440111066673
